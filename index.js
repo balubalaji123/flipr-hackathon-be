6fc6f1e631677f945bb56111c2251aba8da061a4
balu balaji <balajipuvvada12289@example.com>
2020-09-13 12:40:38 +0530

    endpoint for task1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express=require('express') 
-const app=express()
-const path=require('path')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const contact = require('./routes/contact');
 
 const cors = require('cors');
 app.use(cors());
@@ -8,9 +10,10 @@ var cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.get('/',function(req,res){
+app.use('/contact', contact);
+app.get('/', function (req, res) {
     res.send("welcome to app")
 })
-app.listen(process.env.PORT || 3000,()=>{ 
+app.listen(process.env.PORT || 3000, () => {
     console.log(`serving website `);
 })
---
+++ routes/contact.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const router = express.Router();
+
+let contacturl = "https://api.rootnet.in/covid19-in/contacts";
+
+let settings = { method: "Get" };
+
+router.get('/', function (req, res) {
+    fetch(contacturl, settings)
+        .then(res => res.json())
+        .then((json) => {
+            res.json(json.data.contacts.regional);
+        });
+})
+module.exports = router;
