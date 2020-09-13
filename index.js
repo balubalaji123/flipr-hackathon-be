70195f5c6890b1c73615355438ab571d7dde5921
balu balaji <balajipuvvada12289@example.com>
2020-09-13 09:58:10 +0530

    index.js file created

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get('/',function(req,res){
+    res.send("welcome to app")
+})
 app.listen(process.env.PORT || 3000,()=>{ 
     console.log(`serving website `);
 })
